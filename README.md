bf720b983bd74cef2650e5c9152b66c01fc5bb8b
Mario Krutzikowsky <mario.krutzikowsky@gmail.com>
2023-10-17 11:40:29 +0200

    Update

--- README.md
+++ README.md
@@ -2,19 +2,11 @@
 
 A Shopping List
 
-## Next Step
-
-- HTTP Request
-- POST Request Firebase
-- Error Handling
 
 # Screens
 
 <div>
   <img src="lib/assets/images/screens/aa.png" alt="A" width="400" />
   <img src="lib/assets/images/screens/bb.png" alt="B" width="400" />
-  <img src="lib/assets/images/screens/cc.png" alt="C" width="400" />
-  <img src="lib/assets/images/screens/dd.png" alt="D" width="400" />
-  <img src="lib/assets/images/screens/ee.png" alt="E" width="400" />
-  <img src="lib/assets/images/screens/ff.png" alt="F" width="400" />
+ 
 </div>
--- lib/widgets/grocdery_list.dart
+++ lib/widgets/grocdery_list.dart
@@ -28,43 +28,47 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https('shopping-list-70e5c-default-rtdb.firebaseio.com',
         'shopping-list.json');
 
-    final response = await http.get(url);
+    try {
+      final response = await http.get(url);
 
-    if (response.statusCode >= 400) {
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = '404 - Humor not found.';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
       setState(() {
-        _error = '404 - Humor not found.';
-      });
-    }
-
-    // Insert Try and Catch
-
-    if (response.body == 'null') {
-      setState(() {
+        _groceryItems = loadedItems;
         _isLoading = false;
       });
-      return;
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong!';
+      });
     }
-
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
-      loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ),
-      );
-    }
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
   }
 
   void _addItem() async {
